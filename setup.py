3983e0760860c18010823560f1783bfa62043009
Jake <jake,gads@gmail.com>
2019-06-29 13:04:15 -0400

    Removed comments from setup

--- setup.py
+++ setup.py
@@ -10,24 +10,19 @@ setup(
   author = 'Jake Gadaleta',
   author_email = 'jake.gads@gmail.com',
   url = 'https://github.com/gadzygadz/pip_install_assistant',
+  download_url = 'https://github.com/gadzygadz/pip_install_assistant/archive/v01.tar.gz'
   keywords = ['pip', 'installer'],
   
-  classifiers=[  # Optional
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stable
+  classifiers=[  
     'Development Status :: 2 - Beta',
 
-    # Indicate who your project is intended for
+    
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
 
-    # Pick your license as you wish
+    
     'License :: OSI Approved :: MIT License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 3',
   ],
 )
