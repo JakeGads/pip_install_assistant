382c2816ff04e64d839e72c57d6ceb5757957daf
Jake <jake,gads@gmail.com>
2019-06-29 14:20:31 -0400

    1.1 Realease with README

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # pip_install_assistant
 A python script to handle pip installs during compile time
 
-<s>
+
 ## Installation [Coming Soon]
 
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install pipper.
 
 ```bash
-pip install pipper
+pip install pip-install-assistant
 ```
-</s>
+
 
 ## Usage
 
 ```bash
-from pipper import installer
+from pip-install-assistant import installer
 try:
     import packagex # this is not a real package
 except
@@ -27,3 +27,4 @@ except
 
 packages - non-null list of packages that you wish to install <br />
 pythons - a nullable list of the python command ie ```python``` or ```python3``` these are also the default cases <br />
+pips - a nullable list of the pip commands ie ```pip``` or ```pip3``` these are also the default cases <br/>
--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup
-
 from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'pip_install_assistant',
   packages = ['pip_install_assistant'],
+  install_requires = ['pip_install_assistant'],
   version = '0.1.0',
   license='MIT',
   description = 'Installs pip packages at runtime',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author = 'Jake Gadaleta',
   author_email = 'jake.gads@gmail.com',
   url = 'https://github.com/gadzygadz/pip_install_assistant',
-  download_url = 'https://github.com/gadzygadz/pip_install_assistant/archive/v01.tar.gz'
+  download_url = 'https://github.com/gadzygadz/pip_install_assistant/archive/v01.tar.gz',
   keywords = ['pip', 'installer'],
   
   classifiers=[  
-    'Development Status :: 2 - Beta',
-
-    
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
 
